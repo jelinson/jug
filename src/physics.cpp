55dd9b20d01bc42d94b686ef63eb9e5aed647215
Julius Elinson <jelinson@hmc.edu>
2013-05-01 14:16:38 -0700

    isReachableStart() check, qasserts

--- src/physics.cpp
+++ src/physics.cpp
@@ -3,26 +3,31 @@
 using namespace cv;
 
 Physics::Physics()
+    : _route(NULL)
 {
 }
 
 bool Physics::isPossible(const ClimberState &pos) const
 {
+    Q_ASSERT(isRouteLoaded());
     return false;
 }
 
 QList<ClimberState> Physics::configurations(const ClimberState &pos) const
 {
+    Q_ASSERT(isRouteLoaded());
     return QList<ClimberState>();
 }
 
 void Physics::loadClimber(const ClimberSpecs &specs)
 {
+    _specs = specs;
 }
 
 
 void Physics::fillInCom(ClimberState &state, Point com) const
 {
+    Q_ASSERT(isRouteLoaded());
     if (com.x != -1 && com.y != -1)
         state._com = com;
     else {
@@ -41,12 +46,32 @@ void Physics::fillInCom(ClimberState &state, Point com) const
     }
 }
 
-bool Physics::isReachableStart(const ClimberState &current)
+bool Physics::isReachableStart(const ClimberState &pos)
 {
+    Q_ASSERT(isRouteLoaded());
 
+    int llegIndex = pos.getGrip(ClimberState::LeftFoot);
+    int rlegIndex = pos.getGrip(ClimberState::RightFoot);
+
+    Q_ASSERT(llegIndex < _route->nGrips());
+    Q_ASSERT(rlegIndex < _route->nGrips());
+
+    bool possible = false;
+    if (llegIndex != -1)
+        possible = possible | ((*_route)[llegIndex]->getCom().y < CS_LIMB_MAX);
+
+    if(rlegIndex != -1)
+        possible = possible | ((*_route)[rlegIndex]->getCom().y < CS_LIMB_MAX);
+
+    return possible;
 }
 
 void Physics::loadRoute(const Route *r)
 {
     _route = r;
+}
+
+bool Physics::isRouteLoaded() const
+{
+    return _route != NULL;
 }
--- src/physics.h
+++ src/physics.h
@@ -14,13 +14,15 @@ public:
     Physics();
     bool isPossible(const ClimberState& pos) const;
     QList<ClimberState> configurations(const ClimberState& pos) const;
-    void loadClimber(const ClimberSpecs& specs);
+    void loadClimber(const ClimberSpecs &specs);
     void loadRoute(const Route* r);
+    bool isRouteLoaded() const;
     void fillInCom(ClimberState &state, cv::Point com=cv::Point(-1, -1)) const;
-    bool isReachableStart(const ClimberState& current);
+    bool isReachableStart(const ClimberState& pos);
 
 private:
     const Route* _route;
+    ClimberSpecs _specs;
 };
 
 #endif // PHYSICS_H
